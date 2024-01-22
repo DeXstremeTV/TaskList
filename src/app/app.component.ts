5695f7872a9923cefd0a3e7ba61a32acb2dc4709
bruno <brunoandreribeiro18@gmail.com>
2024-01-22 21:14:48 +0000

    added download todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListComponent, CommonModule],
+  imports: [RouterOutlet, ListComponent, CommonModule, BrowserModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodoModel } from '../../model/todo-items-model';
 import { CommonModule } from '@angular/common';
+import * as fs from 'fs';
 
 @Component({
   selector: 'app-list',
@@ -35,5 +36,16 @@ export class ListComponent {
   // edit task
 
   // save to file
+  downloadTodos(): void {
+    const jsonData = JSON.stringify(this.todo, null, 2); 
+    const filePath = `${__dirname}/todo.txt`; 
 
+    // Convert the TodoModel to JSON format
+    const todoJson = JSON.stringify(this.todo, null, 2);
+
+    // Write the JSON to the file
+    fs.writeFileSync(filePath, todoJson, 'utf-8');
+    console.log("clicked")
+  }
+  
 }
