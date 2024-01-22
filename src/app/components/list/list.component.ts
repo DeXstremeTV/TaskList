13e90aad8ea882793e5445c56449764546ab7b49
bruno <brunoandreribeiro18@gmail.com>
2024-01-22 19:06:28 +0000

    style and functionality

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TodoModel } from '../../model/todo-items-model';
 import { CommonModule } from '@angular/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Component({
   selector: 'app-list',
@@ -28,5 +30,22 @@ export class ListComponent {
     item.done = !item.done;
   }
 
+  // add task
+
+  // delete task
+
+  // edit task
+
+  // save to file
+  downloadTodos(): void {
+    const jsonData = JSON.stringify(this.todo, null, 2); 
+    const filePath = path.join(__dirname); // Assuming the file is in the same directory as the script
+
+    // Convert the TodoModel to JSON format
+    const todoJson = JSON.stringify(this.todo, null, 2);
+  
+    // Write the JSON to the file
+    fs.writeFileSync(filePath, todoJson);
+  }
 
 }
