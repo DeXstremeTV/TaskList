3cd9535f5d1ff800b9790844f42d2828d3997aa4
bruno <brunoandreribeiro18@gmail.com>
2024-01-25 21:37:25 +0000

    added authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ListComponent } from './components/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: RegisterComponent,
+        title: "Register"
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: "Login"
+    },
+    {
+        path: 'todo',
+        component: ListComponent,
+        title: "Todo"
+    },
+];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { Login } from '../../model/login';
 import { JwtAuth } from '../../model/jwt-auth';
 import { AuthenticationService } from '../../services/authentication.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   providers: [AuthenticationService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -16,11 +18,14 @@ export class LoginComponent {
   loginDto = new Login();
   jwtDto = new JwtAuth();
 
-  constructor( private authService: AuthenticationService) {}
+  constructor( private authService: AuthenticationService, private router: Router) {}
 
   login(loginDto: Login){
     this.authService.login(loginDto).subscribe((jwtDto) => {
       localStorage.setItem('jwtToken', jwtDto.token);
+      if(jwtDto.result){
+        this.router.navigate(["todo"]);
+      }
     });
   }
 }
